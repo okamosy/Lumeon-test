abba2506cdef331fb13f80c306cf4e43bc83ea55
Seiji Okamoto <Seiji.Okamoto@informa.com>
2017-01-29 21:33:44 +0000

    Handle assigning patient to non-existing doctor.

--- src/AppBundle/Controller/DoctorController.php
+++ src/AppBundle/Controller/DoctorController.php
@@ -41,4 +41,25 @@ class DoctorController extends Controller
         );
     }
 
+    /**
+     * @Route("/assign/{doctorId}/{patientId}")
+     *
+     * @param int $doctorId
+     * @param int $patientId
+     *
+     * @return JsonResponse
+     */
+    public function AssignAction( $doctorId = 0, $patientId = 0 )
+    {
+        $doctor = $this->getDoctrine()->getRepository( 'AppBundle:Doctor' )->selectById( $doctorId );
+        if( empty( $doctor ) ) {
+            return new JsonResponse(
+                [
+                    'msg' => 'Doctor not found'
+                ],
+                 404
+            );
+        }
+        return new JsonResponse();
+    }
 }
