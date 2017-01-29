b6e1e8a22ab7c5f632df93a8d257bfefa0d1e7e3
Seiji Okamoto <Seiji.Okamoto@informa.com>
2017-01-29 12:59:28 +0000

    Add Doctor entity to Patient

--- src/AppBundle/Entity/Patient.php
+++ src/AppBundle/Entity/Patient.php
@@ -2,113 +2,160 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
 class Patient
 {
-	const GENDER_MALE = 1;
-	const GENDER_FEMALE = 2;
-	const GENDER_OTHER = 3;
+    const GENDER_MALE   = 1;
+    const GENDER_FEMALE = 2;
+    const GENDER_OTHER  = 3;
 
-	/** @var  int */
-	private $id;
-	/** @var  string */
-	private $name;
-	/** @var  \DateTime */
-	private $dob;
-	/** @var  string */
-	private $gender;
-	/** @var  Hospital */
-	private $hospital;
+    /** @var  int */
+    private $id;
+    /** @var  string */
+    private $name;
+    /** @var  \DateTime */
+    private $dob;
+    /** @var  string */
+    private $gender;
 
-	/**
-	 * @return int
-	 */
-	public function getId()
-	{
-		return $this->id;
-	}
+    /**
+     * @var  Hospital
+     *
+     * @ORM\ManyToOne(targetEntity="Hospital", inversedBy="patients")
+     * @ORM\JoinColumn(name="hospitalId", referencedColumnName="id")
+     */
+    private $hospital = null;
 
-	/**
-	 * @param int $id
-	 * @return Patient
-	 */
-	public function setId($id)
-	{
-		$this->id = $id;
-		return $this;
-	}
+    /**
+     * @var Doctor
+     *
+     * @ORM\ManyToOne(targetEntity="Doctor", inversedBy="patients")
+     * @ORM\JoinColumn(name="doctorId", referencedColumnName="id")
+     */
+    private $doctor = null;
 
-	/**
-	 * @return string
-	 */
-	public function getName()
-	{
-		return $this->name;
-	}
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @param string $name
-	 * @return Patient
-	 */
-	public function setName($name)
-	{
-		$this->name = $name;
-		return $this;
-	}
+    /**
+     * @param int $id
+     *
+     * @return Patient
+     */
+    public function setId( $id )
+    {
+        $this->id = $id;
 
-	/**
-	 * @return \DateTime
-	 */
-	public function getDob()
-	{
-		return $this->dob;
-	}
+        return $this;
+    }
 
-	/**
-	 * @param \DateTime $dob
-	 * @return Patient
-	 */
-	public function setDob($dob)
-	{
-		$this->dob = $dob;
-		return $this;
-	}
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getGender()
-	{
-		return $this->gender;
-	}
+    /**
+     * @param string $name
+     *
+     * @return Patient
+     */
+    public function setName( $name )
+    {
+        $this->name = $name;
 
-	/**
-	 * @param string $gender
-	 * @return Patient
-	 */
-	public function setGender($gender)
-	{
-		$this->gender = $gender;
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return Hospital
-	 */
-	public function getHospital()
-	{
-		return $this->hospital;
-	}
+    /**
+     * @return \DateTime
+     */
+    public function getDob()
+    {
+        return $this->dob;
+    }
 
-	/**
-	 * @param Hospital $hospital
-	 * @return Patient
-	 */
-	public function setHospital($hospital)
-	{
-		$this->hospital = $hospital;
-		return $this;
-	}
+    /**
+     * @param \DateTime $dob
+     *
+     * @return Patient
+     */
+    public function setDob( $dob )
+    {
+        $this->dob = $dob;
 
+        return $this;
+    }
 
+    /**
+     * @return string
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param string $gender
+     *
+     * @return Patient
+     */
+    public function setGender( $gender )
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * @return Hospital
+     */
+    public function getHospital()
+    {
+        return $this->hospital;
+    }
+
+    /**
+     * @param Hospital $hospital
+     *
+     * @return Patient
+     */
+    public function setHospital( $hospital )
+    {
+        $this->hospital = $hospital;
+
+        return $this;
+    }
+
+    /**
+     * Sets the current doctor.
+     *
+     * @param Doctor $doctor
+     *
+     * @return Patient
+     */
+    public function setDoctor( Doctor $doctor )
+    {
+        $this->doctor = $doctor;
+
+        return $this;
+    }
+
+    /**
+     * Gets the current doctor.
+     *
+     * @return null|Doctor
+     */
+    public function getDoctor()
+    {
+        return $this->doctor;
+    }
 }
