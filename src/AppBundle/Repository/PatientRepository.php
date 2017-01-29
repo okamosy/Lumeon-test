d8b975def281e4813455e056f6b3de36fe1fcc02
Seiji Okamoto <Seiji.Okamoto@informa.com>
2017-01-29 20:20:51 +0000

    Add code to fetch Patient object

--- src/AppBundle/Repository/PatientRepository.php
+++ src/AppBundle/Repository/PatientRepository.php
@@ -2,20 +2,37 @@
 
 namespace AppBundle\Repository;
 
-
 use AppBundle\Entity\Patient;
+use Doctrine\ORM\EntityRepository;
 
-class PatientRepository implements RepositoryInterface
+class PatientRepository extends EntityRepository implements RepositoryInterface
 {
-	/** @return Patient */
-	public function selectById($id)
-	{
-		// TODO: Implement selectById() method.
-	}
+    /**
+     * @param $id
+     *
+     * @return bool|Patient
+     */
+    public function selectById( $id )
+    {
+        $result = $this->getEntityManager()
+                       ->createQuery( 'SELECT p from AppBundle:Patient p WHERE p.id = :id' )
+                       ->setParameter( 'id', $id )
+                       ->getResult();
 
-	/**
-	 * @param \AppBundle\Entity\Hospital $hospital
-	 * @return Patient[]
-	 */
-	public function selectByHospital($hospital){}
+        if( empty( $result[0] ) )
+        {
+            return false;
+        }
+
+        return $result[0];
+    }
+
+    /**
+     * @param \AppBundle\Entity\Hospital $hospital
+     *
+     * @return Patient[]
+     */
+    public function selectByHospital( $hospital )
+    {
+    }
 }
