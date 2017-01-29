d9dad76576723d9f3e4e5507ed2cf26d9326a23a
Seiji Okamoto <Seiji.Okamoto@informa.com>
2017-01-29 18:21:59 +0000

    Fix doctor repository fetch to only return false, or a single result

--- src/AppBundle/Repository/DoctorRepository.php
+++ src/AppBundle/Repository/DoctorRepository.php
@@ -2,14 +2,25 @@
 
 namespace AppBundle\Repository;
 
+use Doctrine\ORM\EntityRepository;
 
 /**
  * DoctorRepository
  */
-class DoctorRepository implements RepositoryInterface
+class DoctorRepository extends EntityRepository implements RepositoryInterface
 {
     public function selectById( $id )
     {
-        // TODO: Implement selectById() method.
+        $result = $this->getEntityManager()
+                    ->createQuery( 'SELECT d from AppBundle:Doctor d WHERE d.id = :id' )
+                    ->setParameter( 'id', $id )
+                    ->getResult();
+
+        if( empty( $result[0] ) )
+        {
+            return false;
+        }
+
+        return $result[0];
     }
 }
