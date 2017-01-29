9c285292707a5ec1e99c50716477a13c76be7dc8
Seiji Okamoto <Seiji.Okamoto@informa.com>
2017-01-29 18:23:18 +0000

    Fix controller response for fetching a doctor

--- src/AppBundle/Controller/DoctorController.php
+++ src/AppBundle/Controller/DoctorController.php
@@ -2,10 +2,12 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Doctor;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * Class DoctorController
@@ -17,20 +19,26 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class DoctorController extends Controller
 {
     /**
-     * @Route("/{id}")
+     * @Route("/{doctorId}")
      *
-     * @param int doctorId
+     * @param int $doctorId
      *
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return JsonResponse
      */
     public function ShowAction( $doctorId = 0)
     {
         $doctor = $this->getDoctrine()->getRepository( 'AppBundle:Doctor' )->selectById( $doctorId );
 
         if( empty( $doctor ) ) {
-            return new JsonResponse( [ 'message' => 'Doctor not found' ], 404 );
+            return new JsonResponse( [ 'message' => 'Doctor ('.$doctorId.') not found' ], 404 );
         }
-        return new JsonResponse();
+
+        $normalizers = array(new ObjectNormalizer());
+
+        $serializer = new Serializer($normalizers, [ new JsonEncoder() ] );
+        return new JsonResponse(
+                $serializer->serialize( $doctor, 'json' )
+        );
     }
 
 }
